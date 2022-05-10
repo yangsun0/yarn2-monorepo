26825136631b0d767931103abc9b50130b9ca8c9
Yang Sun <yangsun@lcodej.com>
2022-05-10 13:54:44 -0700

    add workspace test

--- packages/api/src/app.test.ts
+++ packages/api/src/app.test.ts
@@ -1,7 +1,7 @@
 import request = require("supertest");
 import app from "./app";
 
-test("test example", async () => {
+test("mydata API should return success", async () => {
   const res = await request(app).get("/api/mydata");
   expect(res.statusCode).toBe(200);
   expect(res.text).toBe('{"text":"hello world 1"}');
